8c6b586cce4480c34f62d07ac7b8ce947b223df9
ruari511 <ruari511@gmail.com>
2017-12-31 20:12:54 +0900

    Initial commit

---
+++ aaa/Assets/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	Rigidbody rigid;
+	public float speed = 1000f;
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigid.AddForce(transform.forward * speed);
+	}
+	void OnCollisionEnter(Collision col)
+	{
+		if(col.gameObject.tag==("Wall"))
+		Destroy(this.gameObject);
+	}
+}
---
+++ aaa/Assets/Gun.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour { //모든 총의 기본이 되는 베이스 클래스
+
+	[SerializeField]
+	int ID; //해당 총기의 고유 id
+
+	public float Gun_Damage; //총의 데미지
+	public float GunSpeaker=3; //총의 연사력
+	public int GunMinload; //총의 장전 수
+	public int GunMaxload = 30; //총의 장탄 수
+	public float GunReloatTime; //총의 재장전 시간
+
+	Transform GunPos; // 총알 생성 위치
+	
+
+
+	public Bullet _bullet;
+
+
+
+	// Use this for initialization
+	void Start () {
+		GunPos = transform.GetChild(0);
+		GunMinload = GunMaxload;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)&&GunMinload>0)
+		{
+			//GunPos = transform.GetChild(0);
+			//	Instantiate(_bullet.gameObject, GunPos.transform.position, Quaternion.identity);
+
+			//	GunMinload--;
+			StartCoroutine("Shoot", GunSpeaker);
+
+		}
+
+		if (Input.GetKeyDown(KeyCode.LeftControl))
+			GunMinload = GunMaxload;
+
+	}
+
+	IEnumerator Shoot(float _gunSpeaker)
+	{
+		for (int i = 0; i < _gunSpeaker; i++)
+		{
+			Instantiate(_bullet.gameObject, GunPos.transform.position, Quaternion.identity);
+			GunMinload--;
+			yield return new WaitForSeconds(1 / _gunSpeaker);
+		}
+		
+	}
+}
